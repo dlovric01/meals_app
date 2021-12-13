a2a9881f3e9d4c7c4006d464879c9dc1bbe29263
dlovric01 <danijellovric01@gmail.com>
2021-12-13 17:15:04 +0100

    routes

--- lib/screens/category_meals.dart
+++ lib/screens/category_meals.dart
@@ -21,6 +21,7 @@ class _CategoryMealsState extends State<CategoryMeals> {
     super.initState();
   }
 
+  @override
   void didChangeDependencies() {
     final routeArgs =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
@@ -41,12 +42,50 @@ class _CategoryMealsState extends State<CategoryMeals> {
       body: ListView.builder(
         itemCount: meals!.length,
         itemBuilder: (ctx, index) {
-          return InkWell(
-            onTap: () {},
-            child: Text(meals![index].title.toString()),
+          var complexity = meals![index].complexity;
+          var affordability = meals![index].affordability;
+
+          return Card(
+            child: ListTile(
+              onTap: () {},
+              leading: Image.network(meals![index].imageUrl, width: 70),
+              title: Text('${meals![index].title}'),
+              subtitle: Text('${meals![index].duration} Minutes  ' +
+                  complexityText(complexity) +
+                  '  ' +
+                  affordabilityText(affordability)),
+            ),
           );
         },
       ),
     );
+  }
+
+  String complexityText(var checkComplexity) {
+    switch (checkComplexity) {
+      case Complexity.Simple:
+        return 'Simple';
+
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Hard:
+        return 'Hard';
+
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String affordabilityText(var checkAffordability) {
+    switch (checkAffordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Pricey:
+        return 'Pricey';
+      case Affordability.Luxurious:
+        return 'Luxurious';
+      default:
+        return 'Unknown';
+    }
   }
 }
